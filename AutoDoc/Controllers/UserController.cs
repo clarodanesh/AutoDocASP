99083004e9c0028dcbfeaadc21c1bba1bf328f19
Danesh Iqbal <44497069+clarodanesh@users.noreply.github.com>
2020-04-26 00:36:33 +0100

    basic create/register added
    need styles - validation still

---
+++ AutoDoc/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoDoc.Models;
+
+namespace AutoDoc.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DisplayUsers()
+        {
+            return View();
+        }
+
+        /*[HttpGet]
+        public ActionResult CheckUserExists()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CheckUserExists()
+        {
+            return View();
+        }*/
+
+        [HttpGet]
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(user model)
+        {
+            if (ModelState.IsValid)
+            {
+                var db = new userEntities();
+                db.users.Add(new user
+                {
+                    email = model.email,
+                    firstname = model.firstname,
+                    lastname = model.lastname,
+                    password = model.password,
+                    dob = model.dob,
+                    utype = model.utype
+                });
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+
+    }
+}
