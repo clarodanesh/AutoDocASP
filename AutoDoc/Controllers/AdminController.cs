487843629aa00b529c700da7f3d8156e30674f61
Danesh Iqbal <44497069+clarodanesh@users.noreply.github.com>
2020-04-30 19:58:07 +0100

    added manage docs

--- AutoDoc/Controllers/AdminController.cs
+++ AutoDoc/Controllers/AdminController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web;
+using System.Linq;
 using System.Web.Mvc;
+using AutoDoc.Models;
+using AutoDoc.ViewModels;
+using System.Web.Helpers;
 
 namespace AutoDoc.Controllers
 {
@@ -11,6 +14,26 @@ namespace AutoDoc.Controllers
         // GET: Admin
         public ActionResult OpenAdminLanding()
         {
+            if (Session["UTYPE"] as string == "ADMIN")
+            {
+                //do something interesting
+
+                //since the user who is logged in is a standard user
+                //open the users landing page
+                return View(/*entities.appointments.ToList()*/);
+
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public ActionResult ManageDoctors()
+        {
+            var entities = new appointmentEntities();
+            //var currentUserEmail = Session["EMAIL"] as string;
+            //var v = entities.appointments.Where(u => u.user.Equals(currentUserEmail)).ToList();
             if (Session["UTYPE"] as string == "ADMIN")
             {
                 //do something interesting
