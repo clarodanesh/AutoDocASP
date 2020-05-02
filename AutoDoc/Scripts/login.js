1d5530586462a6d8691231c0aff8d05558009fdf
Danesh Iqbal <44497069+clarodanesh@users.noreply.github.com>
2020-05-02 17:45:10 +0100

    js validation for login
    adding js validation so that there is a trifecta of html validation, server side validation and js validation

---
+++ AutoDoc/Scripts/login.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    console.log("ready!");
+
+    $("#login-btn").click(function (e) {
+        let email = $('#email').val();
+        let password = $('#password').val();
+
+        if (ADValidation.ValidateEmail(email) && ADValidation.ValidatePassword(password)) {
+            return true;
+        } else {
+            e.preventDefault();
+            $('#auth-form-error').html('Email or Password is incorrect');
+        }
+    });
+});
---
+++ AutoDoc/Scripts/validation.js
@@ -0,0 +1,27 @@
+﻿var ADValidation = {
+    libName: "Auto Doc Validation"
+};
+
+ADValidation.ValidateEmail = function (email) {
+    if (email.match("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+ADValidation.ValidatePassword = function (password) {
+    if (password.length > 6) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+ADValidation.ValidateName = function (name) {
+    if (name.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
